d6445810ed170d0886a22fa0b85013f43e4682f3
BoroaDev7 <jmborjas@unitec.edu>
2022-02-04 14:01:32 -0600

    subiendo proyecto

---
+++ src/laboratorio3/Clases.java
@@ -0,0 +1,41 @@
+
+package laboratorio3;
+
+
+public class Clases {
+    
+    private String nombredeclase;
+    private String codigodeclase;
+
+    public Clases() {
+    }
+
+    public Clases(String nombredeclase, String codigodeclase) {
+        this.nombredeclase = nombredeclase;
+        this.codigodeclase = codigodeclase;
+    }
+
+    public String getNombredeclase() {
+        return nombredeclase;
+    }
+
+    public void setNombredeclase(String nombredeclase) {
+        this.nombredeclase = nombredeclase;
+    }
+
+    public String getCodigodeclase() {
+        return codigodeclase;
+    }
+
+    public void setCodigodeclase(String codigodeclase) {
+        this.codigodeclase = codigodeclase;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre de la Clase = " + nombredeclase + " Coodigo de Clase = " + codigodeclase ;
+    }
+    
+    
+            
+}
